34ff0536ea6dbbf0e7d6d14ebd27cb96321c54ca
Harun Aşık <harun.esad@gmail.com>
2022-07-08 00:04:45 +0300

    commit

--- Assets/Scripts/PlanetNameShow.cs
+++ Assets/Scripts/PlanetNameShow.cs
@@ -7,27 +7,33 @@ public class PlanetNameShow : MonoBehaviour
 {
     public TMP_Text planetName;
     public Button closeText;
+    Vector3 mousePos, lastMousePos;
     void Start()
     {
         
     }
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
         {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit))
+            if (hit.transform != null)
             {
-                if (hit.transform != null)
+                if (Input.GetMouseButtonDown(0))
                 {
                     Debug.Log(hit.transform.name);
-                    planetName.gameObject.SetActive(true); 
+                    planetName.gameObject.SetActive(true);
                     closeText.gameObject.SetActive(true);
                     planetName.text = hit.transform.name;
                 }
             }
         }
+        //mousePos = ray.direction;
+        lastMousePos = Input.mousePosition;
+        mousePos = Input.mousePosition - lastMousePos;
+        lastMousePos = Input.mousePosition;
+        Debug.Log(mousePos);
     }
     public void TextClose()
     {
